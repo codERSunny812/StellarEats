2fd281a77625bf651e9201e4597f691483fc8607
codersunny812 <sengersunny448@gmail.com>
2023-11-06 02:43:33 +0530

    main component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/header/Header'
+import Body from './Components/Body/Body'
+import Footer from './Components/Footer/Footer'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <Body/>
+     <Footer />
+    </>
+  )
+}
+
+export default App
