94533d0177cbe6c166a49ed8689de4970a5f9ed0
codersunny812 <sengersunny448@gmail.com>
2023-11-06 02:45:08 +0530

    footer component

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+    <h1>this is footer</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit facilis beatae velit exercitationem aperiam maiores autem dolore inventore, error, provident earum atque distinctio molestias eum pariatur? Vel saepe ab iure obcaecati totam, perferendis quia veritatis quaerat cupiditate. Quae ut, inventore reprehenderit laboriosam rerum nam temporibus animi eos ipsum soluta nihil impedit optio mollitia? In harum ut nihil beatae, ratione quas ad voluptate inventore veniam maiores saepe corporis ipsa animi! Quisquam sit sed sint repellat quam consequuntur dolore, corrupti culpa nobis cum atque iste reiciendis dignissimos assumenda adipisci? Quos, dolore? Hic dolor consequatur voluptate. Laudantium corporis enim delectus cupiditate et quisquam repellat expedita, ipsum fugiat, assumenda, deleniti tempore consectetur id fuga at laborum reprehenderit voluptatibus iusto accusamus aspernatur quam qui. Modi, assumenda aliquam.</p>
+    </div>
+  )
+}
+
+export default Footer
