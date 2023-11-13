609d1754d1105efd03eb72dfd2f976b147d0824a
codersunny812 <sengersunny448@gmail.com>
2023-11-13 17:11:35 +0530

    header component css

---
+++ src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchBar.css'
+
+const SearchBar = () => {
+  return (
+    <div className='searchBar'>
+        <input type="search" name="" id="" />
+    </div>
+  )
+}
+
+export default SearchBar
