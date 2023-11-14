7ab1cec0634b416c44e1dbb0353b06dfb7750c30
codersunny812 <sengersunny448@gmail.com>
2023-11-14 16:23:27 +0530

    constant variable file

--- src/constant.jsx
+++ src/constant.jsx
@@ -1531,3 +1531,5 @@
 
  
  export const imageId = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const URL ="https://www.swiggy.com/mapi/homepage/getCards?lat=26.7877845&lng=80.9677658";
