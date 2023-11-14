6484cfe06b7665274f85a5ef24ba8dfeed34d836
codersunny812 <sengersunny448@gmail.com>
2023-11-14 16:22:18 +0530

    fetch data is added to the body

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,37 +1,56 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './body.scss';
 import './Body.css';
-import { restaurantList } from '../../constant';
+import { URL, restaurantList } from '../../constant';
 import RestaurantCard from '../RestaurantCard/RestaurantCard';
 import SearchBar from '../SearchBar/SearchBar'
-
-
-
+import Shimmer from '../Shimmmer/Shimmer';
 
 
 
 const Body = () => {
-  const [restaurants,setRestaurant]=useState(restaurantList);
+
+  const [restaurants,setRestaurant]=useState([]);
+
   const  filterRestaurant = (searchTerm) =>{
     const filteredRestaurant = restaurantList.filter(
       (ele) => ele.info.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setRestaurant(filteredRestaurant);
   };
+
+// call api 
+useEffect(()=>{
+  getRestaurant();
+},[])
+
+async function getRestaurant(){
+  const data = await fetch(URL);
+  const json = await data.json();
+  // console.log(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+  setRestaurant(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+}
   return (
     <>
     <div className="searchCont">
         <SearchBar onSearch={filterRestaurant} />
       </div>
+    
     <div className='bodyCard'>
         {
+         
             restaurants.map((element)=>{
                 return(
                     <RestaurantCard  card={element.info} delivaryInfo={element.info.sla} key={element.info.id} />
+                    
                 );
+              
+             
             })
+           
         }
     </div>
+
     </>
   );
 }
