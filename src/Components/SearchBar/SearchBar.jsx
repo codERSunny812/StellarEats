46639c5e08a6f16186e0ee95891f47b8015f2cb5
codersunny812 <sengersunny448@gmail.com>
2023-11-14 09:20:52 +0530

    search bar component

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './SearchBar.css'
+// SearchBar.js
+import React, { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
+  const [word, setWord] = useState("");
+
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    setWord(searchTerm);
+    onSearch(searchTerm);
+  };
+
   return (
-    <div className='searchBar'>
-        <input type="search" name="" id="" />
+    <div className="searchBar">
+      <input
+        type="search"
+        className="searchpanel"
+        placeholder="Search your Food"
+        value={word}
+        onChange={handleInputChange}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
