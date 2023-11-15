25c9bcb2b7dd311cc1af509f6846873299298506
codersunny812 <sengersunny448@gmail.com>
2023-11-15 18:35:56 +0530

    shimmer is added

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './body.scss';
-import './Body.css';
+import './body.css';
 import { URL, restaurantList } from '../../constant';
 import RestaurantCard from '../RestaurantCard/RestaurantCard';
 import SearchBar from '../SearchBar/SearchBar'
@@ -10,47 +10,56 @@ import Shimmer from '../Shimmmer/Shimmer';
 
 const Body = () => {
 
-  const [restaurants,setRestaurant]=useState([]);
+  const [restaurants, setRestaurant] = useState(null);
 
-  const  filterRestaurant = (searchTerm) =>{
-    const filteredRestaurant = restaurantList.filter(
+  const filterRestaurant = (searchTerm) => {
+    if(restaurants == null){
+      return;
+    }
+    const filteredRestaurant = restaurants.filter(
       (ele) => ele.info.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setRestaurant(filteredRestaurant);
   };
 
-// call api 
-useEffect(()=>{
-  getRestaurant();
-},[])
+  // call api 
+  useEffect(() => {
+    getRestaurant();
+  }, [])
 
-async function getRestaurant(){
-  const data = await fetch(URL);
-  const json = await data.json();
-  // console.log(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
-  setRestaurant(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
-}
+  async function getRestaurant() {
+    const data = await fetch(URL);
+    const json = await data.json();
+    // console.log(json);
+    // console.log(json?.data?.success?.cards.length);
+    // setRestaurant(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+    if (json?.data?.success?.cards.length == 5){
+    setRestaurant(json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+    }
+    else{
+      setRestaurant(json?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants);
+    }
+  }
   return (
     <>
-    <div className="searchCont">
-        <SearchBar onSearch={filterRestaurant} />
+      <div className="searchCont">
+        <SearchBar searchFun={filterRestaurant} />
       </div>
-    
-    <div className='bodyCard'>
-        {
-         
-            restaurants.map((element)=>{
-                return(
-                    <RestaurantCard  card={element.info} delivaryInfo={element.info.sla} key={element.info.id} />
-                    
-                );
-              
-             
-            })
-           
-        }
-    </div>
 
+      <div className='bodyCard'>
+
+        {
+          restaurants != null ? (
+          restaurants.map((element) => {
+            return (
+              <RestaurantCard card={element.info} delivaryInfo={element.info.sla} key={element.info.id} />
+            )
+          })
+          ) : <Shimmer/>
+
+        }
+      </div>
+     
     </>
   );
 }
