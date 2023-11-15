dc4680af4ece4610c34214f54e1d5e1f0ec56247
codersunny812 <sengersunny448@gmail.com>
2023-11-15 18:34:50 +0530

    Router is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Header from './Components/header/Header'
 import Body from './Components/Body/Body'
 import Footer from './Components/Footer/Footer'
+import {createBrowserRouter} from 'react-router-dom'
+import Help from './Components/Help/Help'
+import Error from './Components/Error/Error'
 
 
 
@@ -21,4 +20,16 @@ function App() {
   )
 }
 
-export default App
+const AppRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>,
+    errorElement:<Error/>
+  },
+  {
+    path:'/help',
+    element:<Help/>
+  }
+]);
+
+export default AppRouter;
