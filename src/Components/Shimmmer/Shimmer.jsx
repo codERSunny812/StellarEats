c5fd3cdbe407c1b8ce2eb0cdfb036d3e761733f4
codersunny812 <sengersunny448@gmail.com>
2023-11-17 19:40:14 +0530

    shimmer

--- src/Components/Shimmmer/Shimmer.jsx
+++ src/Components/Shimmmer/Shimmer.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './Shimmer.css'
-
+import shimmer from './shimmer.json'
+import Lottie from 'lottie-react'
 const Shimmer = () => {
   return (
    <div className="bodyCard">{
   Array(25).fill("").map(()=>(
     <div className="shimmerCard">
-      <div className="img"></div>
-      <div className="name"></div>
+       <div className="img"></div>
+       <div className="name"></div>
       <div className="avgrating"></div>
-      <div className="delivarytime"></div>
-      <div className="cuisines"></div>
-      <div className="areaName"></div>
-    </div>
+       <div className="delivarytime"></div>
+       <div className="cuisines"></div>
+       <div className="areaName"></div>
+      {/* <Lottie animationData={shimmer} className='shimmeranim' /> */}
+
+     </div>
+    
   ))
       }   </div>
   )
