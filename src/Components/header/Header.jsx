175a83d0f590b921947fae0e5bacb319b5a331a1
codersunny812 <sengersunny448@gmail.com>
2023-11-18 20:12:44 +0530

    header responsive

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,41 +1,60 @@
-import React from 'react'
-import './header.scss'
-import './header.css'
-import { BsSearch } from 'react-icons/bs';
-import { BiSolidOffer } from 'react-icons/bi'
+import React from "react";
+import "./header.css";
+import { BsSearch } from "react-icons/bs";
+import { BiSolidOffer } from "react-icons/bi";
 import { IoHelpBuoyOutline } from "react-icons/io5";
-import { BiUserCircle } from 'react-icons/bi'
-import { AiOutlineShoppingCart } from 'react-icons/ai'
-import {Link} from 'react-router-dom';
+import { BiUserCircle } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
- 
 const Logo = () => {
-  return ( 
-  <Link href="/" >
-      <img src="https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png" alt="logo" className='logo_image' />
-  </Link>
-  
-  )
-}
+  return (
+    <Link href="/">
+      <img
+        src="https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png"
+        alt="logo"
+        className="logo_image"
+      />
+    </Link>
+  );
+};
 
-const Header = ({ onSearchClick }) => {
+const Header = () => {
   return (
     <>
-    <div className="header">
-    <Logo className="header_image"/>
-    <ul>
-        
-         
-          <li onClick={onSearchClick} > <BsSearch /><span>search</span>   </li>
-          <li><BiSolidOffer /> <span>offer</span></li>
-          <Link to='/help' className='link'><li><IoHelpBuoyOutline /><span>help</span></li>   </Link>
-          <Link to='/signin' className='link'><li><BiUserCircle /><span>sign in</span></li> </Link>
-          <Link to='/cart' className='link'><li><AiOutlineShoppingCart /><span>cart</span></li></Link >
-    </ul>
-    </div>
+      <div className="header">
+        <Logo className="header_image" />
+        <ul>
+          <li>
+            {" "}
+            <BsSearch />
+            <span>search</span>{" "}
+          </li>
+          <li>
+            <BiSolidOffer /> <span>offer</span>
+          </li>
+          <Link to="/help" className="link">
+            <li>
+              <IoHelpBuoyOutline />
+              <span>help</span>
+            </li>{" "}
+          </Link>
+          <Link to="/signin" className="link">
+            <li>
+              <BiUserCircle />
+              <span>sign in</span>
+            </li>{" "}
+          </Link>
+          <Link to="/cart" className="link">
+            <li>
+              <AiOutlineShoppingCart />
+              <span>cart</span>
+            </li>
+          </Link>
+        </ul>
+      </div>
     </>
-    
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
