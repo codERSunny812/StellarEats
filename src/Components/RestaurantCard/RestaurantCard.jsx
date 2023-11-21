8a72c3d107ea4f3b05345eb09f50cbfe47d9d0df
codersunny812 <sengersunny448@gmail.com>
2023-11-21 19:36:08 +0530

    card component

--- src/Components/RestaurantCard/RestaurantCard.jsx
+++ src/Components/RestaurantCard/RestaurantCard.jsx
@@ -1,30 +1,33 @@
-import { imageId } from '../../constant';
-import { AiFillStar } from 'react-icons/ai'
-import './RestaurantCard.css'
+import { imageId } from "../../constant";
+import { AiFillStar } from "react-icons/ai";
+import "./RestaurantCard.css";
 
 const RestaurantCard = (props) => {
-    // console.log(restaurant);
-    // console.log(props);
-    const { card } = props;
-    // console.log(card);
+  // console.log(restaurant);
+  // console.log(props);
+  const { card } = props;
+  // console.log(card);
 
-    const { cloudinaryImageId, name, cuisines, avgRating, areaName, sla } = card;
+  const { cloudinaryImageId, name, cuisines, avgRating, areaName, sla } = card;
 
-
-    return (
-        <div className='card'>
-            <img src={imageId + cloudinaryImageId} alt="food_logo" />
-            <h2>{name}</h2>
-            <div className="newCont">
-                <AiFillStar color='green' />
-                <h4>{avgRating}</h4>
-                <h4>{sla.deliveryTime}mins</h4>
-            </div>
-            <h3>{cuisines.join(" , ")}</h3>
-            <h5>{areaName}</h5>
-
+  return (
+    <div className="card">
+      <img src={imageId + cloudinaryImageId} alt="food_logo" />
+      <h2>{name}</h2>
+      <div className="newCont">
+        <div className="Cardrating">
+          <AiFillStar color="white" />
+          <h4>{avgRating}</h4>
         </div>
-    );
-}
+
+        <h4 className="delivaryTime">
+          {sla.deliveryTime} <span>mins</span>
+        </h4>
+      </div>
+      <h3 style={{ fontSize: "13px" }}>{cuisines.join(" , ")}</h3>
+      <h5>{areaName}</h5>
+    </div>
+  );
+};
 
 export default RestaurantCard;
