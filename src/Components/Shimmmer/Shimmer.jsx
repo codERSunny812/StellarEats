d3364a168ae7565b3f9af645ca4c74f3cedd31f7
codersunny812 <sengersunny448@gmail.com>
2023-11-21 19:39:30 +0530

    shimmer component

--- src/Components/Shimmmer/Shimmer.jsx
+++ src/Components/Shimmmer/Shimmer.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import './Shimmer.css'
+import React from "react";
+import "./Shimmer.css";
 const Shimmer = () => {
   return (
-   <div className="bodyCard">{
-  Array(25).fill("").map(()=>(
-    <div className="shimmerCard">
-       <div className="img"></div>
-       <div className="name"></div>
-      <div className="avgrating"></div>
-       <div className="delivarytime"></div>
-       <div className="cuisines"></div>
-       <div className="areaName"></div>
-     </div>
-    
-  ))
-      }   </div>
-  )
-}
+    <div className="bodyCard">
+      {Array(25)
+        .fill("")
+        .map(() => (
+          <div className="shimmerCard">
+            <div className="img"></div>
+            <div className="name"></div>
+            <div className="avgrating"></div>
+            <div className="delivarytime"></div>
+            <div className="cuisines"></div>
+            <div className="areaName"></div>
+          </div>
+        ))}{" "}
+    </div>
+  );
+};
 
-export default Shimmer
+export default Shimmer;
