23271e7a5802c7a0cf7fa8cc26c386c96e1f896b
codersunny812 <sengersunny448@gmail.com>
2023-11-21 19:31:35 +0530

    discount component

--- src/Components/Restaurant Menu/Discount.jsx
+++ src/Components/Restaurant Menu/Discount.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './discountInfo.css'
-import { BiSolidOffer } from 'react-icons/bi'
+import React from "react";
+import "./discountInfo.css";
+import { BiSolidOffer } from "react-icons/bi";
 
 const Discount = (props) => {
-    // console.log(props);
-    const {discountInfo} = props;
-    // console.log(discountInfo);
+  // console.log(props);
+  const { discountInfo } = props;
+  // console.log(discountInfo);
 
-    return(
-        <>
-            {discountInfo.map((item) => (
-                <div className="discountBox" key={item.id}>
-                    <BiSolidOffer className='offerIcon'/>
-                    <h5>{item?.meta}</h5>
-                </div>
-            ))}
-        </>     
-    );
-  }
+  return (
+    <>
+      {discountInfo.map((item) => (
+        <div className="discountBox" key={item.id}>
+          <BiSolidOffer className="offerIcon" />
+          <h5>{item?.meta}</h5>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default Discount;
