f4dadd471aedd09617611ba64f81d7adf13d32aa
codersunny812 <sengersunny448@gmail.com>
2023-11-21 19:41:32 +0530

    main rendering component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AppRouter from './App'
-import { RouterProvider } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRouter from "./App";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={AppRouter}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={AppRouter} />
+  </React.StrictMode>
+);
