1d20a15112027814e6dffb0edcc1c8343234be70
codersunny812 <sengersunny448@gmail.com>
2023-11-21 19:27:42 +0530

    no restaurant component

--- src/Components/NoRestaurant/NoRestaurant.jsx
+++ src/Components/NoRestaurant/NoRestaurant.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import './noRestaurant.css'
-import animation from '../animation.json'
-import Lottie from 'lottie-react'
-import './noRestaurant.css'
+import React from "react";
+import "./noRestaurant.css";
+import animation from "../animation.json";
+import Lottie from "lottie-react";
+import "./noRestaurant.css";
 
 const NoRestaurant = () => {
   return (
-   <>
-   <div className="container">
-    <Lottie animationData={animation} className='anim'/>
-    <div className="text">
-    <h1>no restaurant found with that name</h1>
-    <p>Try to search other restaurants!!!!!</p>
-    </div>
-   </div>
-   </>
-  )
-}
+    <>
+      <div className="container">
+        <Lottie animationData={animation} className="anim" />
+        <div className="text">
+          <h1>no restaurant found with that name</h1>
+          <p>Try to search other restaurants!!!!!</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default NoRestaurant
+export default NoRestaurant;
