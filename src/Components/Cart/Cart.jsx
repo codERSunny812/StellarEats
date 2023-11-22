1e945ac5fbeab86a442e002e0c9ab5f20aa9053b
codersunny812 <sengersunny448@gmail.com>
2023-11-22 14:14:05 +0530

    cart component

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import Lottie from "lottie-react";
+import cartAnim from './cartAnim.json'
 
 const Cart = () => {
-  return <div>Cart</div>;
+  return <div> <Lottie animationData={cartAnim} /></div>;
 };
 
 export default Cart;
