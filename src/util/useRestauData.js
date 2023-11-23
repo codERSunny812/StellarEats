f7fe6f2cc79429c20d99a03e088994ec7a1737b9
codersunny812 <sengersunny448@gmail.com>
2023-11-23 22:05:04 +0530

    utility files

--- src/util/useRestauData.js
+++ src/util/useRestauData.js
@@ -1,8 +1,35 @@
 
 
 const useRestauData = () =>{
+    const [filtredRestaurant, setFiltredRestaurant] = useState(null);
+    const [allrestaurant, setAllRestaurant] = useState(null);
 
-    //iska logic likhna hai 
+    // Api call to get the restaurant data
+    useEffect(() => {
+        getRestaurant();
+    }, []);
+
+    async function getRestaurant() {
+        try {
+            const data = await fetch(URL);
+            const json = await data.json();
+            setFiltredRestaurant(
+                json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle
+                    ?.restaurants
+            );
+            setAllRestaurant(
+                json?.data?.success?.cards[4]?.gridWidget?.gridElements?.infoWithStyle
+                    ?.restaurants
+            );
+            // console.log(allrestaurant);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    }
+
+    return {allrestaurant,filtredRestaurant};
+
+
 
 }
 
