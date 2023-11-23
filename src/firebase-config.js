28ee5db795b738216506e88b6b8d4bcf4c3a50e3
codersunny812 <sengersunny448@gmail.com>
2023-11-23 22:06:08 +0530

    firebase config

---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyByalC_uIXhp0bG3W2VY-zLpexsg7egvlU",
+    authDomain: "freshfood-ab9c1.firebaseapp.com",
+    projectId: "freshfood-ab9c1",
+    storageBucket: "freshfood-ab9c1.appspot.com",
+    messagingSenderId: "309816565987",
+    appId: "1:309816565987:web:24be525f8d9e15dd5765d6",
+    measurementId: "G-ZYKD77C3SB"
+};
+
+// create the con
+const App = initializeApp(firebaseConfig);
