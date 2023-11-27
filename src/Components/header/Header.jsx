ca379322f0a9d2f700eb64329bc9fc7efe2e5e2d
codersunny812 <sengersunny448@gmail.com>
2023-11-27 15:31:56 +0530

    header component

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import "./header.css";
 import { BsSearch } from "react-icons/bs";
 import { BiSolidOffer } from "react-icons/bi";
@@ -6,10 +6,14 @@ import { IoHelpBuoyOutline } from "react-icons/io5";
 import { BiUserCircle } from "react-icons/bi";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Store from "../../util/store";
+import { SearchContext } from "../Context/SearchContext";
+
 
 const Logo = () => {
   return (
-    <Link href="/">
+    <Link to="/">
       <img
         src="https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png"
         alt="logo"
@@ -20,12 +24,24 @@ const Logo = () => {
 };
 
 const Header = () => {
+  
+// subscribing to  our store
+const cartItem = useSelector(Store => Store.cart.items);
+const SearchCont = useContext(SearchContext);
+
+  const SearchToggleFun = () =>{
+    
+    SearchCont.isSearchVisible
+      ? SearchCont.setIsSearchVisible(false)
+      : SearchCont.setIsSearchVisible(true);
+  }
+
   return (
     <>
       <div className="header">
         <Logo className="header_image" />
         <ul>
-          <li>
+          <li onClick={SearchToggleFun}>
             <BsSearch />
             <span>search</span>
           </li>
@@ -50,7 +66,7 @@ const Header = () => {
           <Link to="/cart" className="link">
             <li>
               <AiOutlineShoppingCart />
-              <span>cart</span>
+              <span>cart {cartItem.length}</span>
             </li>
           </Link>
         </ul>
