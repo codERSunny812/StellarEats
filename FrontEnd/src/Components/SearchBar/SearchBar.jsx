5fc9028b33e4ffa02decf9200a46548b7621e0ac
codersunny812 <sengersunny448@gmail.com>
2023-12-10 22:32:46 +0530

    search bar

---
+++ FrontEnd/src/Components/SearchBar/SearchBar.jsx
@@ -0,0 +1,27 @@
+// SearchBar.js
+import React, { useState } from "react";
+import "./searchBar.css";
+
+const SearchBar = ({ searchFun }) => {
+  const [word, setWord] = useState("");
+
+  const handleInputChange = (e) => {
+    const searchTerm = e.target.value;
+    setWord(searchTerm);
+    searchFun(searchTerm);
+  };
+
+  return (
+    <div className="searchBar">
+      <input
+        type="search"
+        className="searchpanel"
+        placeholder="Search your Food"
+        value={word}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
