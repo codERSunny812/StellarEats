cd5c68977a7e952a8f2d9e68b07ea47ea6faf965
codersunny812 <sengersunny448@gmail.com>
2023-12-10 22:24:54 +0530

    constant  file

---
+++ FrontEnd/src/constant.js
@@ -0,0 +1,11 @@
+export const imageId =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const menuURL =
+  "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.8546202&lng=79.8886084&restaurantId=";
+
+export const URL = "https://www.swiggy.com/mapi/homepage/getCards?lat=27.8546202&lng=79.8886084";
+
+// export const URL =
+//   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.8546202&lng=80.9677658&page_type=DESKTOP_WEB_LISTING";
+
