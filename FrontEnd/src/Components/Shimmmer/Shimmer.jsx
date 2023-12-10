089ff6bbde9d32319d8ef1ac3b53aa71638f4c74
codersunny812 <sengersunny448@gmail.com>
2023-12-10 22:30:38 +0530

    shimmer component

---
+++ FrontEnd/src/Components/Shimmmer/Shimmer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Shimmer.css";
+const Shimmer = () => {
+  return (
+    <div className="bodyCard">
+      {Array(8)
+        .fill("")
+        .map(() => (
+
+          <div className="shimmerCard" key={Math.random()}>
+            <div className="img"></div>
+            <div className="name"></div>
+            <div className="avgrating"></div>
+            <div className="delivarytime"></div>
+            <div className="cuisines"></div>
+            <div className="areaName"></div>
+          </div>
+        ))}{" "}
+    </div>
+  );
+};
+
+
+
+export default Shimmer;
