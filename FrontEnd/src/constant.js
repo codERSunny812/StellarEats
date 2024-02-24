5187771ca03974e86aa00e8a2156f107023df184
codersunny812 <sengersunny448@gmail.com>
2024-02-24 11:01:07 +0530

    ui updated

--- FrontEnd/src/constant.js
+++ FrontEnd/src/constant.js
@@ -5,7 +5,7 @@ export const menuURL =
   "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=27.8546202&lng=79.8886084&restaurantId=";
 
 export const URL =
-  "https://www.swiggy.com/mapi/homepage/getCards?lat=27.8546202&lng=79.8886084";
+  "https://mocki.io/v1/9db5e6dc-841a-44b3-a3c7-94c53603e11b";
 
 // export const URL =
 //   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.8546202&lng=80.9677658&page_type=DESKTOP_WEB_LISTING";
